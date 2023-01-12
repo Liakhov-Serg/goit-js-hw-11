f98bb48e608fb26ff61603cd0f8027bc844c9140
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-01-12 20:10:31 +0200

    Update index.js
    20-10

--- src/index.js
+++ src/index.js
@@ -6,4 +6,11 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+// new SimpleLightbox(`.card a`, {
+//     `captions`:true,
+//     `captionsData`:`alt`,
+
+// }
+
+// )
 console.log(555);
