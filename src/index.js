acbcb2e15070dc93d926ec5535d8105cec11da86
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-01-12 19:39:23 +0200

    19-40
    111

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+
+// import ApiServ from ``;
+// import createCard from ``;
+import Notiflix from 'notiflix';
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(555);
-import ApiServ from ``;
-import createCard from ``;
-import Notiflix from ``;
