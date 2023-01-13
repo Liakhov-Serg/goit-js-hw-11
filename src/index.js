cb001a71de9f0aaa5040b419597deb34f1432264
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-01-13 13:56:15 +0200

    Update index.js
    111

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 // import {ApiServ} from "./galleryAPI";
 // import createCard from ``;
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
@@ -27,62 +27,139 @@ refs.moreBtn.addEventListener('submit', inMoreLoad);
 
 let value = "";
 let currentPage = 1;
-
+let currentPer_page = 40;
 
 function inSearch(e) {
     e.preventDefault();
    
     value = e.target.elements.searchQuery.value;
-    console.log(value);
+    // console.log(value);
     
-   // getGallery(value).then(data => console.log(data.data.hits));
-    //console.log(getGallery);
+   getGallery(value).then(data => console.log(data.data.hits));
+    console.log(getGallery);
     // clearContainer();
     // try {
     //     const card = await getGallery(value).then(res => {
     //        // console.log(data.data.hits);
     //         renderCard(res.data.hits);
     //     })
-        
-        
+    refs.moreBtn.hidden = false;
+    currentPage = 1;
+     e.target.reset(); 
+
+     clearContainer();
+
+     getCard(value);
+
+
+
+  //refs.loadMoreBtn.disabled = false;    
+
+    // console.log(galleryApi);
+    // if (galleryApi.value === '')
+    //     return console.log("Sorry, there are no images matching your search query. Please try again.") 
+}
+async function getCard(value) {
+  try {
+       const res = await getGallery(value, currentPage)
+           //console.log(res.data.hits);
+         //console.log(res.data.totalHits);
+
+         if (res.data.hits.length > 0 && checkSpaces(value)) {
+           renderCard(res.data.hits);
+           return res;
+         } else {
+           throw new Error('404');
+         }
+
+   } 
+   catch (error) {
+    // Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+
+ }
+ return {}
+}
+
+function inMoreLoad() {
+ currentPage += 1;
+
+
+ getCard(value).then((res) => {
+   console.log(res);
+   let total_pages = res.data.totalHits / currentPer_page;
+
+   Notify.success(`Hooray! We found ${res.data.totalHits} images.`);
+   // console.log(currentPage);
+   // console.log(total_pages);
+   if (currentPage >= total_pages) {
+     refs.loadMoreBtn.hidden = true;
+     Notify.failure("We're sorry, but you've reached the end of search results.");
+   }
+ })
+
+
+}
     // } catch (error) {
     //    // console.log("error")
     // }
-      
+    function renderCard(hits) {
+      const markup = hits.map((({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+          return `<div class="photo-card">
+    <img src="${webformatURL}" data-src="${largeImageURL}" alt="${tags}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes:</b> ${likes}
+      </p>
+      <p class="info-item">
+        <b>Views:</b> ${views}
+      </p>
+      <p class="info-item">
+        <b>Comments:</b> ${comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads:</b> ${downloads}
+      </p>
+    </div>
+    </div>`
+  
+      })).join('');
+  
+      refs.listEl.insertAdjacentHTML('beforeend', markup);
+  } 
     
     
     // console.log(galleryApi);
     // if (galleryApi.value === '')
     //     return console.log("Sorry, there are no images matching your search query. Please try again.") 
-}
 
-function renderCard(hits) {
-    const markup = hits.map((({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-        return `<div class="photo-card">
-  <img src="${webformatURL}" data-src="${largeImageURL}" alt="${tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>${likes}</b>
-    </p>
-    <p class="info-item">
-      <b>${views}</b>
-    </p>
-    <p class="info-item">
-      <b>${comments}</b>
-    </p>
-    <p class="info-item">
-      <b>${downloads}</b>
-    </p>
-  </div>
-</div>`
+
+// function renderCard(hits) {
+//     const markup = hits.map((({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+//         return `<div class="photo-card">
+//   <img src="${webformatURL}" data-src="${largeImageURL}" alt="${tags}" loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">
+//       <b>${likes}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>${views}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>${comments}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>${downloads}</b>
+//     </p>
+//   </div>
+// </div>`
             
-    })).join('');
-    refs.listElem.insertAdjacentHTML('beforeend', markup);
-}
+//     })).join('');
+//     refs.listElem.insertAdjacentHTML('beforeend', markup);
+// }
 function clearContainer() {
-    refs.listElem.innerHTML = ''; 
-    
+  refs.listElem.innerHTML = ''; 
+  
 }
 function checkSpaces(string) {
-  return string.trim() !== '';
+return string.trim() !== '';
 }
