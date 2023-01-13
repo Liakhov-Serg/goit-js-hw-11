0fb380a07af755f36b1b4e5a1d397eb4809e0d3c
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-01-13 13:07:12 +0200

    Update galleryAPI.js
    13-00

--- src/galleryAPI.js
+++ src/galleryAPI.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = `32810341-69b544567d84cbcabae0736a6`;
 
-export function getGallery(value, page) {
+export function getGallery(value, page, per_page) {
     const config = {
             key: API_KEY,
             q: value,
@@ -11,7 +11,7 @@ export function getGallery(value, page) {
             orientation: "horizontal",
         safesearch: "true",
         page: page,
-        per_page: 40,
+        per_page: per_page,
         
     }
  const response = axios.get(`${BASE_URL}`,{ params: config });
