5f1d8036b7c5c6988689ad8af4ed449df064007d
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-01-14 20:36:08 +0200

    Update index.js
    20-35

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
 import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
 import {getGallery} from "./galleryAPI";
